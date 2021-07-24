b5f70df29049550637d10a66b131f4a8d9e5f7f1
Anastasiia Masiian <masiian.anastasiia@chnu.edu.ua>
2021-07-24 15:33:13 +0300

    Unsuccessful controller testing. SCSS errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,9 +25,12 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: "Product was successfully created." }
+        format.html { 
+          redirect_to @product,
+                      notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
+        # puts @product.errors.full_messages
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
@@ -38,7 +41,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: "Product was successfully updated." }
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,19 +54,19 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
-    end
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
+  end
 end
