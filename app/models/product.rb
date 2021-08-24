11faa9d8ebf6b49ba1c7bec37dd9219cc8ac09f2
Anastasiia Masiian <masiian.anastasiia@chnu.edu.ua>
2021-08-24 10:07:34 +0300

    CSS for table rendering Flash notice of an error Removing a parameter from the permitted list

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,8 @@
 #---
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_of_line_items
 # 
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
@@ -15,5 +17,12 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  def ensure_not_referenced_by_any_of_line_items
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 
 end
