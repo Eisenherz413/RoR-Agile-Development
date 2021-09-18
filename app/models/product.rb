342f9f2755abeb392a75da7855721fe884273e07
Anastasiia Masiian <masiian.anastasiia@chnu.edu.ua>
2021-09-18 17:56:13 +0300

    Chapter 12 Payment

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_of_line_items
 # 
   validates :title, uniqueness: true
